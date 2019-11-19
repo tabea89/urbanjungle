4e702f9cd5a8e2339a966e7ca73bef3a21b42be4
tasim <t.simon@s-v.de>
2019-11-19 23:09:32 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header__wrapper">
+    <h1 class="header__title">Urban Jungle</h1>
+  </div>
+</template>
+<script>
+export default {
+    name: "header"
+}
+</script>
+<style lang="sass" scoped>
+    .header 
+        &__wrapper
+            width: 100%
+        &__title
+            margin: auto
+</style>
